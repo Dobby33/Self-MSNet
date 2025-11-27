92651a6753d5e70400d0dc90fda8554a967e70ed
Dobby33 <lnguo@emails.bjut.edu.cn>
2025-11-27 15:10:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 
+if __name__ == '__main__'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_set", type=str)
+    args = parser.parse_args()
+    data_set = args.data_set
+    begin_train(load_model=False, need_to_save_the_process=True, data_set=data_set)
